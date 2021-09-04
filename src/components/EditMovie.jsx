2282ef397fd499a9da93a8a6a981ffdf3add7bf1
Siraj <sgsdbs@gmail.com>
2021-09-04 16:03:07 +0300

    Привязал данные формы к переменным

--- src/components/EditMovie.jsx
+++ src/components/EditMovie.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+//TODO Переделать все под функциональную компоненту
+//TODO с использованием хуков
 export default class EditMovie extends React.Component {
     state = {
         movie: {},
@@ -7,14 +9,44 @@ export default class EditMovie extends React.Component {
         error: null
     }
 
-    componentDidMount() {
-        this.setState({
+
+    constructor(props) {
+        super(props);
+        this.state = {
             movie: {
-                title: "The test",
-                mpaa_rating: "R",
-            }
-        })
+                id: 0,
+                title: "",
+                release_date: "",
+                runtime: "",
+                mpaa_rating: "",
+                rating: "",
+                description: "",
+
+            },
+            isLoaded: false,
+            error: null
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    handleSubmit = (evt) => {
+        console.log("Form was submitted");
+        evt.preventDefault();
+    }
+
+    handleChange = (evt) => {
+        let value = evt.target.value;
+        let name = evt.target.name;
+        this.setState((prevState) => ({
+            movie: {
+                ...prevState.movie,
+                [name]: value,
+            }
+        }));
+    }
+
 
     render() {
         let { movie } = this.state;
@@ -22,7 +54,8 @@ export default class EditMovie extends React.Component {
             <>
                 <h2>Add/Edit Movie</h2>
                 <hr />
-                <form method="post">
+                <form onSubmit={this.handleSubmit}>
+                    <input type="hidden" name="id" id="id" value={movie.id} onChange={this.handleChange} />
                     <div className="mb-3">
                         <label htmlFor="title" className="form-label" >Title</label>
                         <input
@@ -31,6 +64,7 @@ export default class EditMovie extends React.Component {
                             id="title"
                             name="title"
                             value={movie.title}
+                            onChange={this.handleChange}
                         ></input>
                     </div>
                     <div className="mb-3">
@@ -41,6 +75,7 @@ export default class EditMovie extends React.Component {
                             id="release_date"
                             name="release_date"
                             value={movie.release_date}
+                            onChange={this.handleChange}
                         ></input>
                     </div>
                     <div className="mb-3">
@@ -51,12 +86,13 @@ export default class EditMovie extends React.Component {
                             id="runtime"
                             name="runtime"
                             value={movie.runtime}
+                            onChange={this.handleChange}
                         ></input>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="mpaa_rating" className="form-label">MPAA Rating</label>
 
-                        <select className="form-select" value={movie.mpaa_rating}>
+                        <select name="mpaa_rating" className="form-select" value={movie.mpaa_rating} onChange={this.handleChange}>
                             <option className="form-select">Choose...</option>
                             <option className="form-select" value="G">G</option>
                             <option className="form-select" value="PG">PG</option>
@@ -73,13 +109,14 @@ export default class EditMovie extends React.Component {
                             id="rating"
                             name="rating"
                             value={movie.rating}
+                            onChange={this.handleChange}
                         ></input>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="description" className="form-label" >Description</label>
-                        <textarea className="form-control" id="description" name="description" rows="3">
-                            {movie.description}
-                        </textarea>
+                        <textarea className="form-control" id="description" name="description" rows="3" value={movie.description} onChange={this.handleChange} />
+
+
                     </div>
 
                     <hr />
@@ -87,6 +124,10 @@ export default class EditMovie extends React.Component {
                     <button className="btn btn-primary" >Save</button>
 
                 </form>
+
+                <div className="mt-3">
+                    <pre>{JSON.stringify(this.state, null, 3)}</pre>
+                </div>
             </>
         )
     }
